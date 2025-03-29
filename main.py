7caf9250bfc2685d24c540a2eeaf28ce26c333cf
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-03-29 10:05:20 -0500

    added fps cap

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60 / 1000)
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
