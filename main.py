a0a5452d30278558bc1d3020837fad84bfff7a17
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-03-30 16:49:52 -0500

    added forward movement

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60 / 1000)
+        dt = clock.tick(60) / 1000
 
     
 if __name__ == "__main__":
