4cbb0613cb4648bfc6262c4b635d1f56faf01af0
sudocbrown99 <cmichaelbrown99@gmail.com>
2025-04-10 17:35:01 -0500

    added shootign logic

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from bullet import Shot
 
 
 def main():
@@ -12,9 +14,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    asteroid = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    Asteroid.containers = (updatable, drawable, asteroid)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
     
@@ -31,6 +35,11 @@ def main():
                 return
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+             if asteroid.collides_with(player):
+                  print("Game over!")
+                  sys.exit()
         
         screen.fill("black")
         
